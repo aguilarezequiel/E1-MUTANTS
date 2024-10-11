60a59bb7842f0d9f05240321be8ee090464e20cf
Ezequiel <83150188+aguilarezequiel@users.noreply.github.com>
2024-10-11 16:16:30 -0300

    Delete src directory

--- src/main/java/desarrollo/primer_parcial/repositories/HumanoRepository.java
+++
@@ -1,9 +0,0 @@
-package desarrollo.primer_parcial.repositories;
-
-import desarrollo.primer_parcial.entities.Humano;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HumanoRepository extends JpaRepository<Humano, Long> {
-}
