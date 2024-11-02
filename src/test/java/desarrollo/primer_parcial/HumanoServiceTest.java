04cd4905761c0f6f3c4d25ffb1468ec5e41da560
Ezequiel <83150188+aguilarezequiel@users.noreply.github.com>
2024-11-02 14:57:17 -0300

    Update HumanoServiceTest.java

--- src/test/java/desarrollo/primer_parcial/HumanoServiceTest.java
+++ src/test/java/desarrollo/primer_parcial/HumanoServiceTest.java
@@ -13,42 +13,110 @@ public class HumanoServiceTest {
     private final HumanoService humanoService = new HumanoService();
 
     @Test
-    public void testIsMutant() throws Exception {
-        // Datos de prueba
+    public void testIsMutant_dnaMutant1() throws Exception {
         HumanoDTO humanoDTO = new HumanoDTO();
         humanoDTO.setDna(new String[] {
-                "ATGCGA",
-                "CAGTGC",
-                "TTGTAT",
-                "AGAAGG",
-                "CCCCTA",
-                "TCACTG"
+                "AAAA", // Secuencia horizontal de A's
+                "CCCC", // Secuencia horizontal de C's
+                "TCAG",
+                "GGTC"
         });
 
-        // Llamada al método que se está probando
         boolean result = humanoService.isMutant(humanoDTO);
-
-        // Verificación de resultados
         assertTrue(result, "El ADN debe ser considerado mutante.");
     }
 
     @Test
-    public void testIsNotMutant() throws Exception {
-        // Datos de prueba
+    public void testIsNotMutant_dnaNoMutant1() throws Exception {
         HumanoDTO humanoDTO = new HumanoDTO();
         humanoDTO.setDna(new String[] {
-                "ATGCGT",
-                "CAGTGC",
-                "TTGTAT",
-                "AGAAGG",
-                "CCCTAA",
-                "TCACTG"
+                "AAAT",
+                "AACC",
+                "AAAC",
+                "CGGG"
         });
 
-        // Llamada al método que se está probando
         boolean result = humanoService.isMutant(humanoDTO);
-
-        // Verificación de resultados
         assertFalse(result, "El ADN debe ser considerado no mutante.");
+    }
+
+    @Test
+    public void testIsMutant_dnaMutant2() throws Exception {
+        HumanoDTO humanoDTO = new HumanoDTO();
+        humanoDTO.setDna(new String[] {
+                "TGAC",
+                "AGCC",
+                "TGAC",
+                "GGTC"
+        });
+
+        boolean result = humanoService.isMutant(humanoDTO);
+        assertTrue(result, "El ADN debe ser considerado mutante.");
+    }
+
+    @Test
+    public void testIsNotMutant_dnaNoMutant2() throws Exception {
+        HumanoDTO humanoDTO = new HumanoDTO();
+        humanoDTO.setDna(new String[] {
+                "AAAA",
+                "AAAA",
+                "AAAA",
+                "AAAA"
+        });
+
+        boolean result = humanoService.isMutant(humanoDTO);
+        assertFalse(result, "El ADN debe ser considerado no mutante.");
+    }
+
+    @Test
+    public void testIsNotMutant_dnaNoMutant3() throws Exception {
+        HumanoDTO humanoDTO = new HumanoDTO();
+        humanoDTO.setDna(new String[] {
+                "TGAC",
+                "ATCC",
+                "TAAG",
+                "GGTC"
+        });
+
+        boolean result = humanoService.isMutant(humanoDTO);
+        assertFalse(result, "El ADN debe ser considerado no mutante.");
+    }
+
+    @Test
+    public void testIsMutant_dnaMutant3() throws Exception {
+        HumanoDTO humanoDTO = new HumanoDTO();
+        humanoDTO.setDna(new String[] {
+                "TCGGTGATA",
+                "TGATCCTTT",
+                "TACGAGTGA",
+                "AAATGTACT",
+                "ACGAGTGCT",
+                "AGACACTAG",
+                "GAACTCCAA",
+                "ACTACGACC",
+                "TGAGTATCC"
+        });
+
+        boolean result = humanoService.isMutant(humanoDTO);
+        assertTrue(result, "El ADN debe ser considerado mutante.");
+    }
+
+    @Test
+    public void testIsMutant_dnaMutant4() throws Exception {
+        HumanoDTO humanoDTO = new HumanoDTO();
+        humanoDTO.setDna(new String[] {
+                "TTTTTTTTT", // Secuencia horizontal de T's
+                "TTTTTTTTT",
+                "TTTTTTTTT",
+                "TTTTTTTTT",
+                "CCGACCATG",
+                "GGCACTCCA",
+                "AGGACACTA",
+                "CAAAGGCAT",
+                "GCAGTCCCC" // Secuencia vertical de C's
+        });
+
+        boolean result = humanoService.isMutant(humanoDTO);
+        assertTrue(result, "El ADN debe ser considerado mutante.");
     }
 }
